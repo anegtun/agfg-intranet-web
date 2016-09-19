50ead8d2b83b3951978585f2120532b1ab44c1ec
nitta18 <nitta18@gmail.com>
2016-09-20 01:22:35 +0200

    Modelo equipas

--- intranet/app/Config/database.php
+++ intranet/app/Config/database.php
@@ -69,22 +69,11 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'database_name',
-		'prefix' => '',
-		//'encoding' => 'utf8',
-	);
-
-	public $test = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
-		'prefix' => '',
+		'host'       => 'localhost',
+		'login'      => 'root',
+		'password'   => 'root',
+		'database'   => 'agfg',
+		'prefix'     => 'agfg_',
 		//'encoding' => 'utf8',
 	);
 }
