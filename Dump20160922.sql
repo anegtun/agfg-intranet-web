f2c4a3d5135ae482b94291baad624707a0e555a1
nitta18 <nitta18@gmail.com>
2016-09-22 00:49:07 +0200

    Dump BD

--- Dump20160922.sql
+++ Dump20160922.sql
@@ -40,6 +40,35 @@ LOCK TABLES `agfg_equipas` WRITE;
 /*!40000 ALTER TABLE `agfg_equipas` DISABLE KEYS */;
 /*!40000 ALTER TABLE `agfg_equipas` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `agfg_users`
+--
+
+DROP TABLE IF EXISTS `agfg_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `agfg_users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `agfg_users`
+--
+
+LOCK TABLES `agfg_users` WRITE;
+/*!40000 ALTER TABLE `agfg_users` DISABLE KEYS */;
+INSERT INTO `agfg_users` VALUES (1,'agfg-admin','$2a$10$BRzX/K3Ds/8dV1X6MsU2QOc3eBvu7J8bxOa3KyIDSWzztU4ggKfdW','admin','2016-09-21 19:38:38','2016-09-21 19:38:38','Administrador');
+/*!40000 ALTER TABLE `agfg_users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -50,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-09-20  1:21:16
+-- Dump completed on 2016-09-22  0:48:27
