7d90920cf5249c0c46790a90067f01c569bce1c2
anegtun <64235746+anegtun@users.noreply.github.com>
2016-11-21 16:06:36 +0100

    Fix login?

--- intranet-cake-3.3/src/Controller/MainController.php
+++ intranet-cake-3.3/src/Controller/MainController.php
@@ -21,6 +21,8 @@ class MainController extends AppController {
     public function login() {
         if ($this->request->is('post')) {
             $user = $this->Auth->identify();
+            print_r($user);
+            die();
             if ($user) {
                 $this->Auth->setUser($user);
                 return $this->redirect($this->Auth->redirectUrl());
--- intranet-cake-3.3/src/Model/Table/UsuariosTable.php
+++ intranet-cake-3.3/src/Model/Table/UsuariosTable.php
@@ -12,10 +12,10 @@ class UsuariosTable extends Table {
 
     public function validationDefault(Validator $validator) {
         return $validator
-            ->notEmpty('username', 'O usuario é obrigatorio')
-            ->notEmpty('password', 'O contrasinal é obrigatorio')
-            ->notEmpty('role', 'O rol é obrigatorio')
-            ->add('role', 'inList', [
+            ->notEmpty('nome_usuario', 'O usuario é obrigatorio')
+            ->notEmpty('contrasinal', 'O contrasinal é obrigatorio')
+            ->notEmpty('rol', 'O rol é obrigatorio')
+            ->add('rol', 'inList', [
                 'rule' => ['inList', ['admin', 'author']],
                 'message' => 'Introduce un valor válido para o rol'
             ]);
