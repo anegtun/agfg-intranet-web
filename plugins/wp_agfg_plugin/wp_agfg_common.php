a85a92e87261f07d06a44b92a9eacfd9ba5b2cb8
nitta18 <64235746+anegtun@users.noreply.github.com>
2019-11-13 00:34:04 +0100

    CSS embebido no plugin

---
+++ plugins/wp_agfg_plugin/wp_agfg_common.php
@@ -0,0 +1,70 @@
+<?php
+
+
+function wp_agfg_common_style() {
+    return '
+        <style>
+            .agfg-clasificacion table tr th {
+                background-color: #c6e15650;
+                text-align: center;
+            } 
+            .agfg-clasificacion table tr td {
+                padding: 6px !important;
+                text-align: center;
+            }
+            .agfg-clasificacion table tr:nth-child(2n-1) td {
+                background-color: #c6e15615;
+            }
+            .agfg-clasificacion table tr {
+                vertical-align: middle;
+            }
+            .agfg-clasificacion table, .agfg-clasificacion table tr td, .agfg-clasificacion table tr th {
+                border: 0;
+            }
+
+
+
+            .agfg-calendario .xornada {
+                clear: both;
+                padding-top: 1em;
+            } 
+            .agfg-calendario .xornada h4 {
+                border-bottom: 1px solid #869643;
+            } 
+            .agfg-calendario .partido {
+                float: left;
+                width: 250px;
+                height: 130px;
+                border: 1px solid #4187b450;
+                border-radius: 0.5em;
+                margin: 0.5em;
+            }
+            .agfg-calendario .partido-big {
+                height: 155px;
+            }
+            .agfg-calendario .partido table {
+                border: 0;
+            }
+            .agfg-calendario .partido table th, .agfg-calendario .partido table td {
+                border: 0 !important;
+                padding: 0.25em !important;
+                vertical-align: middle;
+            }
+            .agfg-calendario .partido table th {
+                font-weight: bold;
+                border-radius: 0.5em 0.5em 0 0;
+                background-color: #4187b450;
+                color: #04395E;
+            } 
+            .agfg-calendario .partido table tr td:nth-child(1) {
+                width:20px;
+            }
+            .agfg-calendario .partido table tr td:nth-child(2) {
+                width:120px;
+            } 
+            .agfg-calendario .partido table tr td:nth-child(3) {
+                text-align: right;
+                padding-right: 0.5em !important;
+            } 
+        </style>';
+}
