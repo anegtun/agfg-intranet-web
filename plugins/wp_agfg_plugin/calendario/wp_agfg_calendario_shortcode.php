525e2d3855735f14a3048b95ccca0a6b4d48415a
nitta18 <64235746+anegtun@users.noreply.github.com>
2019-11-29 17:07:41 +0100

    Shortcode para amosa partidos da seguinte xornada

--- plugins/wp_agfg_plugin/calendario/wp_agfg_calendario_shortcode.php
+++ plugins/wp_agfg_plugin/calendario/wp_agfg_calendario_shortcode.php
@@ -97,14 +97,4 @@ function wp_agfg_calendario_shortcode($atts) {
     return $html;
 }
 
-function format_resultado($equipa) {
-    $resultado = "";
-    if(!is_null($equipa->goles) || !is_null($p->equipa1->goles)) {
-        $resultado = sprintf('%01d', $equipa->goles)."-".sprintf('%02d', $equipa->tantos)." (".sprintf('%02d', $equipa->total).")";
-    } else {
-        $resultado = "(".sprintf('%02d', $equipa->total).")";
-    }
-    return $resultado;
-}
-
 add_shortcode('agfg-calendario', 'wp_agfg_calendario_shortcode');
