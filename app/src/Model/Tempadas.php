6ecf36f2fb9f7336c643215e57e195acb7be05fc
anegtun <nitta18@gmail.com>
2021-08-30 14:30:08 +0200

    Tempada 21/22

--- app/src/Model/Tempadas.php
+++ app/src/Model/Tempadas.php
@@ -5,6 +5,7 @@ class Tempadas {
     
     public function getTempadas() {
         return array(
+            '2021-22' => '2021/22',
             '2020-21' => '2020/21',
             '2019-20' => '2019/20',
             '2018-19' => '2018/19',
